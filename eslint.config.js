ca06e0f9185ad1a94b8c31438721cacaeca83805
pjt <26487010+PierreJeanjacquot@users.noreply.github.com>
2026-01-07 17:15:48 +0100

    chore: init ts project (#2)

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import unicorn from 'eslint-plugin-unicorn';
+import prettier from 'eslint-config-prettier/flat';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  unicorn.configs.recommended,
+  prettier,
+  {
+    rules: {
+      'no-console': 'error',
+      'unicorn/filename-case': 'off',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+    },
+  },
+
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.esm.json',
+      },
+    },
+  }
+);
