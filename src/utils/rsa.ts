7be9a28ad7c7e6917028422061eda97047f11918
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2026-01-13 09:33:52 +0100

    feat: add RSA utils

---
+++ src/utils/rsa.ts
@@ -0,0 +1,61 @@
+import { bytesToHex } from './bytesToHex.js';
+import { hexToBytes } from './hexToBytes.js';
+
+/**
+ * Generate RSA key pair
+ *
+ * @returns An object containing the public and private keys in PEM format
+ */
+export async function generateRsaKeyPair(): Promise<CryptoKeyPair> {
+  const keyPair = await crypto.subtle.generateKey(
+    {
+      name: 'RSA-OAEP',
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: 'SHA-256',
+    },
+    true,
+    ['decrypt']
+  );
+  return keyPair;
+}
+
+/**
+ * Export RSA public key to spki (DER) hex encoded format
+ *
+ * @param keyPair
+ * @returns spki (DER) hex encoded string public key
+ */
+export async function exportRsaPublicKey({
+  publicKey,
+}: {
+  publicKey: CryptoKey;
+}): Promise<string> {
+  const publicKeyBuffer = await crypto.subtle.exportKey('spki', publicKey);
+  return bytesToHex(new Uint8Array(publicKeyBuffer));
+}
+
+/**
+ * Decrypts ciphertext using RSA-OAEP
+ *
+ * @param privateKey - The RSA private key
+ * @param ciphertextHex - The hex encoded ciphertext to decrypt
+ * @returns Hex encoded decrypted plaintext
+ */
+export async function rsaDecrypt({
+  privateKey,
+  ciphertext,
+}: {
+  privateKey: CryptoKey;
+  ciphertext: string;
+}): Promise<string> {
+  const ciphertextBytes = hexToBytes(ciphertext);
+  const decryptedBuffer = await crypto.subtle.decrypt(
+    {
+      name: 'RSA-OAEP',
+    },
+    privateKey,
+    ciphertextBytes
+  );
+  return bytesToHex(new Uint8Array(decryptedBuffer));
+}
