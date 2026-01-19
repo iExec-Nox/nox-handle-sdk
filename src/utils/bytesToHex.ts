6f6b18f9098d01a16f236a8bb0052eef2efb5ec1
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2026-01-19 14:50:37 +0100

    refactor: merge hex utils in the same module

--- src/utils/bytesToHex.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Converts Uint8Array to hex string with "0x" prefix
- */
-export function bytesToHex(bytes: Uint8Array): string {
-  return '0x' + [...bytes].map((b) => b.toString(16).padStart(2, '0')).join('');
-}
